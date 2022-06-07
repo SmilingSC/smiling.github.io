821841f8b4d2cc5f1ab366e2fa2d4e7bfe5d06d8
SmilingSC <100347428+SmilingSC@users.noreply.github.com>
2022-06-07 21:34:28 +0530

    Add files via upload

---
+++ calculatar.py
@@ -0,0 +1,17 @@
+class Method:
+    def add(x,y):
+        return x+y
+    def sub(x,y):
+        return x-y
+    def multiple(x,y):
+        return x*y
+    def div(x,y):
+        return(x/y)
+    
+n=1
+while n!=0:
+    print("0. Exit")
+    print("1. Add")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
